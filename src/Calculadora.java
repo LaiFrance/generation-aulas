e44fef9b9ae744e0a13ba47727c51800430a2a38
Laise France <laise94@hotmail.com>
2023-02-09 10:04:13 -0300

    aula 2 operadore matematicos

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Calculadora {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
-        float n1, n2;
+        double n1, n2;
         System.out.println("digite o 1 numero:");
-        n1 = leia.nextFloat();
+        n1 = leia.nextDouble();
 
         System.out.println("digite o 2 numero:");
-        n2 = leia.nextFloat();
+        n2 = leia.nextDouble();
+
+        System.out.println("O numero" + n1 + "elevado ao numero" + n2 + "é igual a" + Math.pow(n1,n2));
+
 
         leia.close();
     }
